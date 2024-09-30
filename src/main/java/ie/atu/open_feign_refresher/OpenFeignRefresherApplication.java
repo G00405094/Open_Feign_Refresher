69b79c22de9dd42547d80a4cf8772a3f676eddcc
G00405094@atu.ie <G00405094@atu.ie>
2024-09-30 16:30:06 +0100

    Set Up

---
+++ src/main/java/ie/atu/open_feign_refresher/OpenFeignRefresherApplication.java
@@ -0,0 +1,13 @@
+package ie.atu.open_feign_refresher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenFeignRefresherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenFeignRefresherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/open_feign_refresher/OpenFeignRefresherApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.open_feign_refresher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenFeignRefresherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
